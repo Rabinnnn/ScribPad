22c9e5d9a844e139ed1eaf40ee0dcf10ac9d94f4
Rabinnnn <otiinibar@gmail.com>
2022-10-30 09:50:45 +0300

    V1.0

--- app/src/main/java/com/signaturecapture/Displayed_Patients_Records.java
+++ app/src/main/java/com/signaturecapture/Displayed_Patients_Records.java
@@ -1,16 +1,13 @@
 package com.signaturecapture;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.res.ResourcesCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 public class Displayed_Patients_Records extends AppCompatActivity {
     public ImageView imageView1;
@@ -20,8 +17,27 @@ public class Displayed_Patients_Records extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_displayed_patients_records);
 
+        MyListData[] myListData = new MyListData[] {
+                new MyListData("January", android.R.drawable.ic_dialog_dialer),
+                new MyListData("February", android.R.drawable.ic_dialog_dialer),
+                new MyListData("March", android.R.drawable.ic_dialog_dialer),
+                new MyListData("April", android.R.drawable.ic_dialog_dialer),
+                new MyListData("May", android.R.drawable.ic_dialog_dialer),
+                new MyListData("June", android.R.drawable.ic_dialog_dialer),
+                new MyListData("July", android.R.drawable.ic_dialog_dialer),
+                new MyListData("August", android.R.drawable.ic_dialog_dialer),
+                new MyListData("September", android.R.drawable.ic_dialog_dialer),
+                new MyListData("October", android.R.drawable.ic_dialog_dialer),
+                new MyListData("November", android.R.drawable.ic_dialog_dialer),
+                new MyListData("December", android.R.drawable.ic_dialog_dialer),
+        };
 
-        imageView1 = findViewById(R.id.imageView1);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        MyListAdapter adapter = new MyListAdapter(myListData);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+      /*  imageView1 = findViewById(R.id.imageViewJan);
 
         imageView1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +46,6 @@ public class Displayed_Patients_Records extends AppCompatActivity {
                 startActivity(i);
 
             }
-        });
+        }); */
     }
 }
